89c5b944af262574c626a54cb9832c0f4f973b67
Alex Gustafsson <alex-gustafsson@live.se>
2021-03-16 15:08:55 +0100

    Rewrite to not expose OpenSSL API, fix memory leaks

--- dh/lib/dh.h
+++ dh/lib/dh.h
@@ -2,16 +2,16 @@
 #define ECDH_H
 
 // Generate a keypair
-// in dh: an empty allocated DH struct.
-// out dh: a DH struct containing dh parameters, public kay, and privet key.
+// out pk: a buffer of at least DH_PUBLICKEYBYTES bytes. Will receive the generated public key.
+// out sk: a buffer of at least DH_PRIVATEKEYBYTES bytes. Will receive the generated private key.
 // returns 0 for success and -1 for error.
-int crypto_dh_keypair(DH *dh);
+int crypto_dh_keypair(unsigned char *pk, unsigned char *sk, unsigned char *p, unsigned char *g);
 
 // Calculate the shared session key.
 // out k: a buffer of at least CRYPTO_BYTES bytes. Will receive the derived session key.
-// in dh: a DH struct containing dh parameters, public kay, and privet key.
-// in pk: a BIGNUM holding a peer's public key to use for the exchange.
+// in sk: a buffer holding a secret key to use for the exchange.
+// in pk: a buffer holding a peer's public key to use for the exchange.
 // returns 0 for success and -1 for error.
-int crypto_dh_enc(unsigned char *k, DH *dh, const BIGNUM *pk);
+int crypto_dh_enc(unsigned char *k, const unsigned char *sk, const unsigned char *pk, unsigned char *p, unsigned char *g);
 
 #endif
--- dh/lib/params.h
+++ dh/lib/params.h
@@ -10,5 +10,12 @@
 #define CRYPTO_BYTES 32
 // The number of bytes required to store the plain session key
 #define CRYPTO_SECRETBYTES 256
+// The number of bytes required to store a public key
+#define CRYPTO_PUBLICKEYBYTES 255
+// The number of bytes required to store a private key
+#define CRYPTO_SECRETKEYBYTES 255
+
+#define CRYPTO_PARAM_P_BYTES 256
+#define CRYPTO_PARAM_G_BYTES 1
 
 #endif
