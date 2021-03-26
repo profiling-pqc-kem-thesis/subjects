e91edbade4ca0b7c56b4c64758df0e2c3f34c697
Alex Gustafsson <alex-gustafsson@live.se>
2021-03-26 15:16:29 +0100

    Make script work cross-platform

--- scripts/calculate-stack-usage.sh
+++ scripts/calculate-stack-usage.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 
-export CFLAGS="-Wstack-usage=1 -Werror=stack-usage=1"
-
-make --always-make --directory "$@" 2>&1 | grep -e '-Werror=stack-usage=' -B1 | grep -o -e "In function '[^']\+'\|stack usage \(is\|might be\) [0-9]\+ bytes" | sed -e "s/In function '\([^']\+\)'\|stack usage \(is\|might be\) \([0-9]\+\) bytes/\1\3/g" | paste -sd ' \n'
+CFLAGS="-Wstack-usage=1 -Werror=stack-usage=1" make --always-make --directory "$@" 2>&1 | grep -e '-Werror=stack-usage=' -B1 | grep -o -e "In function .[a-zA-Z_0-9]\+.\|stack usage \(is\|might be\) \([0-9]\+ bytes\|unbounded\)" | sed -e "s/In function .\([a-zA-Z_0-9]\+\).\|stack usage \(is\|might be\) \([0-9]\+ bytes\|unbounded\)/\1\3/g" | paste -sd ' \n'
