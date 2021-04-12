a8c7559710bebe1a833156d238197f20d6e67ac2
Carl Stensson <stensson.carl@hotmail.com>
2021-04-12 09:13:12 +0200

    Add parse time

--- scripts/parse_data.py
+++ scripts/parse_data.py
@@ -1,6 +1,8 @@
 import re
 import sqlite3
 import sys
+from typing import TextIO
+from datetime import datetime
 from os import listdir
 from pathlib import Path
 from shutil import copyfile
@@ -9,7 +11,7 @@ connection = None
 cursor = None
 
 
-def init_data_base():
+def init_database():
     global connection
     connection = sqlite3.connect("data.db")
     global cursor
@@ -29,7 +31,7 @@ def init_data_base():
                 duration UNSIGNED BIG INT)''')
 
 
-def copy_environment_data_base(path: Path):
+def copy_environment_database(path: Path):
     copyfile(path.joinpath("environment.sqlite"), "data.db")
 
 
@@ -46,10 +48,26 @@ def parse_algorithm(file_name: str) -> int:
     return cursor.lastrowid
 
 
-def parse_benchmark(environment: int, benchmark_type: str, algorithm: int, benchmark: int, total_duration: int, start_timestamp: int, stop_timestamp: int):
+def parse_benchmark(environment: int, benchmark_type: str, algorithm: int, benchmark: int, start_timestamp: int, stop_timestamp: int):
     cursor.execute(
         "INSERT INTO benchmark(environment, type, algorithm, benchmark, totalDuration, startTimestamp, stopTimestamp) VALUES (?, ?, ?, ?, ?, ?, ?)",
-        (environment, benchmark_type, algorithm, benchmark, total_duration, start_timestamp, stop_timestamp))
+        (environment, benchmark_type, algorithm, benchmark, stop_timestamp - start_timestamp, start_timestamp, stop_timestamp))
+
+
+def get_times(file: TextIO) -> (int, int):
+    time_pattern = re.compile("[0-9\-]{10} [0-9\:]{8}")
+
+    lines = file.read().splitlines()
+    start_time = re.search(time_pattern, lines[0]).group(0)
+    start_time = datetime.strptime(start_time, "%Y-%m-%d %H:%M:%S") - datetime(1900, 1, 1)
+    start_time = int(start_time.total_seconds())
+
+    stop_time = re.search(time_pattern, lines[-1]).group(0)
+    stop_time = datetime.strptime(stop_time, "%Y-%m-%d %H:%M:%S") - datetime(1900, 1, 1)
+    stop_time = int(stop_time.total_seconds())
+    file.seek(0)
+
+    return start_time, stop_time
 
 
 def parse_sequential(path: Path):
@@ -61,32 +79,67 @@ def parse_sequential(path: Path):
     for file_name in listdir(sequential_path):
         algorithm_id = parse_algorithm(file_name)
         with open(sequential_path.joinpath(file_name)) as input_file:
+            start_time, stop_time = get_times(input_file)
             results = False
             summary = False
             for line in input_file:
                 if line.strip() == "iteration,duration (ns)":
                     results = True
-                    continue
                 elif line.strip() == "=== Summary ===":
                     results = False
                     summary = True
-                    continue
                 elif re.search(end_pattern, line) is not None:
                     break
                 elif results:
                     parts = line.split(",")
                     cursor.execute("INSERT INTO sequentialBenchmarkIteration(sequentialBenchmark, iteration, duration) VALUES (?, ?, ?)", (0, int(parts[0]), int(parts[1])))
+                    parse_benchmark(-1, "sequential", algorithm_id, -999, start_time, stop_time)
                 elif summary:
                     res = re.search(average_pattern, line)
-                    if res is not None:
-                        print(res.group(0))
-                    #parts = line.split(",")
-                    #cursor.execute("INSERT INTO sequentialBenchmark(benchmark, iterations, averageDuration) VALUES (?, ?, ?)", (0, int(parts[0]), int(parts[1])))
+                    if res is None:
+                        continue
+                    iterations = int(re.search("of [0-9]+ iterations", res.group(0)).group(0).split(" ")[1])
+                    average_duration = float(re.search("[0-9\.]+ms", res.group(0)).group(0)[:-2])
+                    cursor.execute("INSERT INTO sequentialBenchmark(benchmark, iterations, averageDuration) VALUES (?, ?, ?)", (-999, iterations, average_duration))
+
+
+def parse_parallel(path: Path):
+    parallel_path = path.joinpath("parallel")
+
+    average_pattern = re.compile("average \(of [0-9]+ iterations\): [0-9\.]+ms")
+    end_pattern = re.compile("=== [0-9\-]{10} [0-9\:]{8} Command ended with exit code 0 ===")
+
+    for file_name in listdir(sequential_path):
+        algorithm_id = parse_algorithm(file_name)
+        with open(sequential_path.joinpath(file_name)) as input_file:
+            start_time, stop_time = get_times(input_file)
+            results = False
+            summary = False
+            for line in input_file:
+                if line.strip() == "iteration,duration (ns)":
+                    results = True
+                elif line.strip() == "=== Summary ===":
+                    results = False
+                    summary = True
+                elif re.search(end_pattern, line) is not None:
+                    break
+                elif results:
+                    parts = line.split(",")
+                    cursor.execute("INSERT INTO sequentialBenchmarkIteration(sequentialBenchmark, iteration, duration) VALUES (?, ?, ?)", (0, int(parts[0]), int(parts[1])))
+                    parse_benchmark(-1, "sequential", algorithm_id, -999, start_time, stop_time)
+                elif summary:
+                    res = re.search(average_pattern, line)
+                    if res is None:
+                        continue
+                    iterations = int(re.search("of [0-9]+ iterations", res.group(0)).group(0).split(" ")[1])
+                    average_duration = float(re.search("[0-9\.]+ms", res.group(0)).group(0)[:-2])
+                    cursor.execute("INSERT INTO sequentialBenchmark(benchmark, iterations, averageDuration) VALUES (?, ?, ?)", (-999, iterations, average_duration))
+
 
 def main():
     path = Path(sys.argv[1])
-    copy_environment_data_base(path)
-    init_data_base()
+    copy_environment_database(path)
+    init_database()
     if connection is None and cursor is None:
         exit(1)
 
